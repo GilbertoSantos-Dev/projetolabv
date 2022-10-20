0d0f7d70230068341f1094150968d0d7141062ff
GilbertoSantos-Dev <80331254+GilbertoSantos-Dev@users.noreply.github.com>
2022-10-20 20:13:21 -0300

    Add files via upload
    Atualização

---
+++ src/main/java/br/gov/sp/fatec/projeetolab5/Projeetolab5Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projeetolab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeetolab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeetolab5Application.class, args);
+	}
+
+}
